0b2e3ddb58189a4073975bc6cf704a5aaf4faedb
Joey <joeyarnstein@gmail.com>
2016-07-14 09:04:43 -0700

    separate logic display list into separate activity DisplaySongsActivity

---
+++ app/src/main/java/com/breakstuff/myrestaurants/ui/DisplaySongsActivity.java
@@ -0,0 +1,92 @@
+package com.breakstuff.myrestaurants.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.breakstuff.myrestaurants.R;
+import com.breakstuff.myrestaurants.services.YelpServices;
+
+import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+//public class DisplaySongsActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_display_songs);
+//    }
+//}
+
+public class DisplaySongsActivity extends AppCompatActivity {
+    public static final String TAG = DisplaySongsActivity.class.getSimpleName();
+
+    @Bind(R.id.textView)
+    TextView mLocationTextView;
+    @Bind(R.id.listView)
+    ListView mListView;
+
+    private String[] songNames = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
+            "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
+            "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_songs);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, songNames);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String song = ((TextView)view).getText().toString();
+                Toast.makeText(DisplaySongsActivity.this, song, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String lyrics = intent.getStringExtra("lyrics");
+
+        mLocationTextView.setText("HShowing results for lyrics: " + lyrics);
+
+        getSongs(lyrics);
+    }
+
+    private void getSongs(String lyrics) {
+        final YelpServices yelpService = new YelpServices();
+        yelpService.findRestaurants(lyrics, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
+
--- app/src/main/java/com/breakstuff/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/breakstuff/myrestaurants/ui/MainActivity.java
@@ -26,8 +26,8 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-    @Bind(R.id.editText) EditText mLocationEditText;
+    @Bind(R.id.findSongButton) Button mFindSongButton;
+    @Bind(R.id.editText) EditText mEditText;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
 
@@ -40,73 +40,73 @@ public class MainActivity extends AppCompatActivity {
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-sans/ostrich-regular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() { //add click listener
+        mFindSongButton.setOnClickListener(new View.OnClickListener() { //add click listener
 
            @Override
             public void onClick(View v) {
-               String location = mLocationEditText.getText().toString();
-               Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-               intent.putExtra("location", location);
+               String lyrics = mEditText.getText().toString();
+               Intent intent = new Intent(MainActivity.this, DisplaySongsActivity.class);
+               intent.putExtra("lyrics", lyrics);
                startActivity(intent);
            }
         });
     }
 
-    public static class RestaurantsActivity extends AppCompatActivity {
-        public static final String TAG = RestaurantsActivity.class.getSimpleName();
-
-        @Bind(R.id.textView) TextView mLocationTextView;
-        @Bind(R.id.listView)
-        ListView mListView;
-
-        private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
-                "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
-                "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_restaurants);
-            ButterKnife.bind(this);
-
-            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
-            mListView.setAdapter(adapter);
-
-            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    String restaurant = ((TextView)view).getText().toString();
-                    Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
-                }
-            });
-
-            Intent intent = getIntent();
-            String location = intent.getStringExtra("location");
-
-            mLocationTextView.setText("Here are all the restaurants near: " + location);
-
-            getRestaurants(location);
-        }
-
-        private void getRestaurants(String location) {
-            final YelpServices yelpService = new YelpServices();
-            yelpService.findRestaurants(location, new Callback() {
-
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    e.printStackTrace();
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    try {
-                        String jsonData = response.body().string();
-                        Log.v(TAG, jsonData);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-    }
+//    public static class RestaurantsActivity extends AppCompatActivity {
+//        public static final String TAG = RestaurantsActivity.class.getSimpleName();
+//
+//        @Bind(R.id.textView) TextView mLocationTextView;
+//        @Bind(R.id.listView)
+//        ListView mListView;
+//
+//        private String[] songNames = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
+//                "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
+//                "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_restaurants);
+//            ButterKnife.bind(this);
+//
+//            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, songNames);
+//            mListView.setAdapter(adapter);
+//
+//            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                    String restaurant = ((TextView)view).getText().toString();
+//                    Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+//                }
+//            });
+//
+//            Intent intent = getIntent();
+//            String location = intent.getStringExtra("location");
+//
+//            mLocationTextView.setText("Here are all the restaurants near: " + location);
+//
+//            getRestaurants(location);
+//        }
+//
+//        private void getRestaurants(String location) {
+//            final YelpServices yelpService = new YelpServices();
+//            yelpService.findRestaurants(location, new Callback() {
+//
+//                @Override
+//                public void onFailure(Call call, IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                @Override
+//                public void onResponse(Call call, Response response) throws IOException {
+//                    try {
+//                        String jsonData = response.body().string();
+//                        Log.v(TAG, jsonData);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
+//        }
+//    }
 }
